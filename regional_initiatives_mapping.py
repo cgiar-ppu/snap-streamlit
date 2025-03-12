25b87616998be46e5f51ea1ba7998d8ceac63a5f
jlbkmve <jlbkmve@gmail.com>
2025-03-12 12:13:31 +0100

    Adjusting input file for post-processing for columns Action Area

--- regional_initiatives_mapping.py
+++ regional_initiatives_mapping.py
@@ -61,7 +61,7 @@ def main():
         'SGP-05': 'ST'     # Adaptation Insights
     }
     
-    input_file = 'input/export_data_table_results_20250403_092607CET.xlsx'
+    input_file = 'input/export_data_table_results_20251203_101413CET.xlsx'
     
     # Read the Excel file
     print("Reading input file...")
